0f9933dd6b6c212c1a0f35abd134eeacbc0ff129
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-06-30 18:02:09 +0300

    added the show-products component

--- src/components/products/ShowProducts.jsx
+++ src/components/products/ShowProducts.jsx
@@ -1,7 +1,99 @@
 import React from "react";
 
+const tableItems = [
+  {
+    name: "Liam James",
+    email: "liamjames@example.com",
+    position: "Software engineer",
+    salary: "$100K",
+  },
+  {
+    name: "Olivia Emma",
+    email: "oliviaemma@example.com",
+    position: "Product designer",
+    salary: "$90K",
+  },
+  {
+    name: "William Benjamin",
+    email: "william.benjamin@example.com",
+    position: "Front-end developer",
+    salary: "$80K",
+  },
+  {
+    name: "Henry Theodore",
+    email: "henrytheodore@example.com",
+    position: "Laravel engineer",
+    salary: "$120K",
+  },
+  {
+    name: "Amelia Elijah",
+    email: "amelia.elijah@example.com",
+    position: "Open source manager",
+    salary: "$75K",
+  },
+];
+
 const ShowProducts = () => {
-  return <div>ShowProducts</div>;
+  return (
+    <>
+      <div className="max-w-screen-xl mx-auto mt-[30px] px-4 md:px-8">
+        <div className="items-start justify-between md:flex">
+          <div className="max-w-lg">
+            <h3 className="text-amber-700 text-xl font-bold sm:text-2xl">
+              Products
+            </h3>
+          </div>
+          <div className="mt-3 md:mt-0">
+            <a
+              href="javascript:void(0)"
+              className="inline-block px-4 py-2 text-white duration-150 font-medium bg-amber-600 rounded-lg hover:bg-amber-700 active:bg-amber-700 md:text-sm"
+            >
+              Add new product
+            </a>
+          </div>
+        </div>
+        <div className="mt-12 shadow-sm border rounded-lg overflow-x-auto">
+          <table className="w-full table-auto text-sm text-left">
+            <thead className="bg-gray-50 text-gray-600 font-medium border-b">
+              <tr>
+                <th className="py-3 px-6">Product Name</th>
+                <th className="py-3 px-6">Price</th>
+                <th className="py-3 px-6">Category</th>
+                <th className="py-3 px-6">Brand</th>
+                <th className="py-3 px-6"></th>
+              </tr>
+            </thead>
+            <tbody className="text-gray-600 divide-y">
+              {tableItems.map((item, idx) => (
+                <tr key={idx}>
+                  <td className="px-6 py-4 whitespace-nowrap">{item.name}</td>
+                  <td className="px-6 py-4 whitespace-nowrap">{item.email}</td>
+                  <td className="px-6 py-4 whitespace-nowrap">{item.salary}</td>
+                  <td className="px-6 py-4 whitespace-nowrap">
+                    {item.position}
+                  </td>
+                  <td className="text-right px-6 whitespace-nowrap">
+                    <a
+                      href="javascript:void()"
+                      className="py-2 px-3 font-medium text-indigo-600 hover:text-indigo-500 duration-150 hover:bg-gray-50 rounded-lg"
+                    >
+                      Edit
+                    </a>
+                    <button
+                      href="javascript:void()"
+                      className="py-2 leading-none px-3 font-medium text-red-600 hover:text-red-500 duration-150 hover:bg-gray-50 rounded-lg"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ShowProducts;
