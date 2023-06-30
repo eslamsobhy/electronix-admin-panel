130a145736b67d839059048da8b1a5cd017c5acd
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-07-01 00:31:25 +0300

    displayed the users coming from the db

--- src/components/products/ShowProducts.jsx
+++ src/components/products/ShowProducts.jsx
@@ -3,39 +3,6 @@ import React from "react";
 import { useGlobalContext } from "../../../context";
 import Loading from "../loading/Loading";
 
-const tableItems = [
-  {
-    name: "Liam James",
-    email: "liamjames@example.com",
-    position: "Software engineer",
-    salary: "$100K",
-  },
-  {
-    name: "Olivia Emma",
-    email: "oliviaemma@example.com",
-    position: "Product designer",
-    salary: "$90K",
-  },
-  {
-    name: "William Benjamin",
-    email: "william.benjamin@example.com",
-    position: "Front-end developer",
-    salary: "$80K",
-  },
-  {
-    name: "Henry Theodore",
-    email: "henrytheodore@example.com",
-    position: "Laravel engineer",
-    salary: "$120K",
-  },
-  {
-    name: "Amelia Elijah",
-    email: "amelia.elijah@example.com",
-    position: "Open source manager",
-    salary: "$75K",
-  },
-];
-
 const ShowProducts = () => {
   const { products, categories, loading } = useGlobalContext().state;
 
