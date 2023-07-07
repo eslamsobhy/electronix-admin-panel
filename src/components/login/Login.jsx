d01b8b946412e4a5d1fabbc9ebbe8b874d76f7e3
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-07-07 21:29:00 +0300

    added validation for the login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import { toast } from "react-toastify";
@@ -6,16 +6,18 @@ import { toast } from "react-toastify";
 import { useGlobalContext } from "../../../context";
 
 const Login = () => {
+  const [invalidCredentials, setInvalidCredentials] = useState(false);
   const { login } = useGlobalContext();
 
   const {
     register,
     handleSubmit,
-    formState: { error },
+    formState: { errors },
     reset,
   } = useForm();
 
   const onSubmit = async (data) => {
+    setInvalidCredentials(false);
     const { email, password } = data;
     try {
       const response = await axios.post(
@@ -26,6 +28,7 @@ const Login = () => {
       localStorage.setItem("token", response.data.token);
       toast.success("Logged in successfully!");
     } catch (e) {
+      setInvalidCredentials(true);
       console.log(e.response?.data.message);
     }
   };
@@ -43,6 +46,11 @@ const Login = () => {
             className="w-[400px]"
           />
           <article className="flex flex-col justify-center items-center gap-[30px]">
+            {invalidCredentials && (
+              <p className="text-red-500" role="alert">
+                Invalid Credentails!
+              </p>
+            )}
             <div className="form-group">
               <label
                 htmlFor="email"
@@ -52,13 +60,30 @@ const Login = () => {
               </label>
 
               <input
-                {...register("email")}
+                {...register("email", {
+                  required: true,
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: "Invalid Email Address",
+                  },
+                })}
+                aria-invalid={errors.email ? "true" : "false"}
                 type="email"
                 id="email"
                 placeholder="john-doe@gmail.com"
                 className="mt-1 rounded-md w-[25rem] border-gray-200 shadow-sm sm:text-sm"
               />
             </div>
+            {errors.email?.type === "required" && (
+              <p className="text-red-500" role="alert">
+                Email is required
+              </p>
+            )}
+            {errors.email?.type === "pattern" && (
+              <p className="text-red-500" role="alert">
+                Email must be valid{" "}
+              </p>
+            )}
             <div className="form-group">
               <label
                 htmlFor="password"
@@ -68,13 +93,20 @@ const Login = () => {
               </label>
 
               <input
-                {...register("password")}
+                {...register("password", {
+                  required: true,
+                })}
                 type="password"
                 id="password"
                 placeholder=""
                 className="mt-1 rounded-md w-[25rem] border-gray-200 shadow-sm sm:text-sm"
               />
             </div>
+            {errors.password?.type === "required" && (
+              <p className="text-red-500" role="alert">
+                Password is required
+              </p>
+            )}
             <div className="mt-3 md:mt-0">
               <button className="inline-block px-4 py-2 text-white duration-150 font-medium bg-amber-600 rounded-lg hover:bg-amber-700 active:bg-amber-700 md:text-sm">
                 Login
