98c8de3c7bf1bd7dc087e7ceaba9f475e986b351
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-07-08 05:02:28 +0300

    added validation for the add-user form

--- src/components/users/AddUser.jsx
+++ src/components/users/AddUser.jsx
@@ -11,7 +11,7 @@ const AddUser = () => {
   const {
     register,
     handleSubmit,
-    formState: { error },
+    formState: { errors },
     reset,
   } = useForm();
 
@@ -52,15 +52,44 @@ const AddUser = () => {
                 className="block text-xs font-medium text-gray-700"
               >
                 First Name
+                <span className="text-[red] relative left-1 text-[16px]">
+                  *
+                </span>
               </label>
 
               <input
-                {...register("first_name")}
+                {...register("first_name", {
+                  required: true,
+                  minLength: 3,
+                  maxLength: 20,
+                  pattern: { value: /^[^0-9].*$/i },
+                })}
+                aria-invalid={errors.first_name ? "true" : "false"}
                 type="text"
                 id="firstName"
                 placeholder="John"
                 className="mt-1 rounded-md w-[25rem] border-gray-200 shadow-sm sm:text-sm"
               />
+              {errors.first_name?.type === "required" && (
+                <p className="text-red-500" role="alert">
+                  First name is required
+                </p>
+              )}
+              {errors.first_name?.type === "minLength" && (
+                <p className="text-red-500" role="alert">
+                  First name must be at least 3 chars
+                </p>
+              )}
+              {errors.first_name?.type === "maxLength" && (
+                <p className="text-red-500" role="alert">
+                  First name must be less than 20 chars
+                </p>
+              )}
+              {errors.first_name?.type === "pattern" && (
+                <p className="text-red-500" role="alert">
+                  Not valid product name
+                </p>
+              )}
             </div>
             <div className="form-group">
               <label
@@ -68,15 +97,44 @@ const AddUser = () => {
                 className="block text-xs font-medium text-gray-700"
               >
                 Last Name
+                <span className="text-[red] relative left-1 text-[16px]">
+                  *
+                </span>
               </label>
 
               <input
-                {...register("last_name")}
+                {...register("last_name", {
+                  required: true,
+                  minLength: 3,
+                  maxLength: 20,
+                  pattern: { value: /^[^0-9].*$/i },
+                })}
+                aria-invalid={errors.last_name ? "true" : "false"}
                 type="text"
                 id="lastName"
                 placeholder="Doe"
                 className="mt-1 rounded-md w-[25rem] border-gray-200 shadow-sm sm:text-sm"
               />
+              {errors.last_name?.type === "required" && (
+                <p className="text-red-500" role="alert">
+                  First name is required
+                </p>
+              )}
+              {errors.last_name?.type === "minLength" && (
+                <p className="text-red-500" role="alert">
+                  First name must be at least 3 chars
+                </p>
+              )}
+              {errors.last_name?.type === "maxLength" && (
+                <p className="text-red-500" role="alert">
+                  First name must be less than 20 chars
+                </p>
+              )}
+              {errors.last_name?.type === "pattern" && (
+                <p className="text-red-500" role="alert">
+                  Not valid product name
+                </p>
+              )}
             </div>
             <div className="form-group">
               <label
@@ -84,15 +142,34 @@ const AddUser = () => {
                 className="block text-xs font-medium text-gray-700"
               >
                 Email
+                <span className="text-[red] relative left-1 text-[16px]">
+                  *
+                </span>
               </label>
 
               <input
-                {...register("email")}
+                {...register("email", {
+                  required: true,
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  },
+                })}
+                aria-invalid={errors.email ? "true" : "false"}
                 type="email"
                 id="email"
                 placeholder="john-doe@gmail.com"
                 className="mt-1 rounded-md w-[25rem] border-gray-200 shadow-sm sm:text-sm"
               />
+              {errors.email?.type === "required" && (
+                <p className="text-red-500" role="alert">
+                  Email is required
+                </p>
+              )}
+              {errors.email?.type === "pattern" && (
+                <p className="text-red-500" role="alert">
+                  Not valid email
+                </p>
+              )}
             </div>
             <div className="form-group">
               <label
@@ -100,15 +177,38 @@ const AddUser = () => {
                 className="block text-xs font-medium text-gray-700"
               >
                 Password
+                <span className="text-[red] relative left-1 text-[16px]">
+                  *
+                </span>
               </label>
 
               <input
-                {...register("password")}
+                {...register("password", {
+                  required: true,
+                  minLength: 6,
+                  maxLength: 20,
+                })}
+                aria-invalid={errors.password ? "true" : "false"}
                 type="password"
                 id="password"
                 placeholder="lnkbgwj_$BRElm2e3w"
                 className="mt-1 rounded-md w-[25rem] border-gray-200 shadow-sm sm:text-sm"
               />
+              {errors.password?.type === "required" && (
+                <p className="text-red-500" role="alert">
+                  Password is required
+                </p>
+              )}
+              {errors.password?.type === "minLength" && (
+                <p className="text-red-500" role="alert">
+                  Password must be at least 6 chars
+                </p>
+              )}
+              {errors.password?.type === "maxLength" && (
+                <p className="text-red-500" role="alert">
+                  Password must be less than 20 chars
+                </p>
+              )}
             </div>
             <div className="form-group">
               <label
@@ -116,15 +216,32 @@ const AddUser = () => {
                 className="block text-xs font-medium text-gray-700"
               >
                 Phone Number
+                <span className="text-[red] relative left-1 text-[16px]">
+                  *
+                </span>
               </label>
 
               <input
-                {...register("phone_number")}
+                {...register("phone_number", {
+                  required: true,
+                  pattern: { value: /^\d{11}$/i },
+                })}
+                aria-invalid={errors.phone_number ? "true" : "false"}
                 type="text"
                 id="phone"
                 placeholder="0101706613"
                 className="mt-1 rounded-md w-[25rem] border-gray-200 shadow-sm sm:text-sm"
               />
+              {errors.phone_number?.type === "required" && (
+                <p className="text-red-500" role="alert">
+                  Phone number is required
+                </p>
+              )}
+              {errors.phone_number?.type === "pattern" && (
+                <p className="text-red-500" role="alert">
+                  Not valid phone number
+                </p>
+              )}
             </div>
             <div className="form-group">
               <label
@@ -132,15 +249,27 @@ const AddUser = () => {
                 className="block text-xs font-medium text-gray-700"
               >
                 Role
+                <span className="text-[red] relative left-1 text-[16px]">
+                  *
+                </span>
               </label>
 
-              <input
-                {...register("role")}
-                type="text"
+              <select
+                {...register("role", { required: true })}
+                aria-invalid={errors.role ? "true" : "false"}
                 id="role"
-                placeholder="admin"
-                className="mt-1 rounded-md w-[25rem] border-gray-200 shadow-sm sm:text-sm"
-              />
+                className="mt-1 w-[25rem] rounded-md border-gray-200 text-gray-700 sm:text-sm"
+              >
+                <option value="">Please select</option>
+                <option value="user">User</option>
+                <option value="admin">Admin</option>
+                <option value="superAdmin">Super admin</option>
+              </select>
+              {errors.role?.type === "required" && (
+                <p className="text-red-500" role="alert">
+                  Role is required
+                </p>
+              )}
             </div>
           </article>
           <div className="my-[30px]">
